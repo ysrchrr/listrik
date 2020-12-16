7e39124652efb49a6164b9e38486f74169af25c9
Roy Aldo <aldorezpector60@gmail.com>
2020-12-16 09:51:22 +0700

    test

--- application/controllers/Customers.php
+++ application/controllers/Customers.php
@@ -1,51 +1,60 @@
 <?php
-class Customers extends CI_Controller{
-    function __construct(){
+class Customers extends CI_Controller
+{
+    function __construct()
+    {
         parent::__construct();
         $this->load->model('Customers_model');
     }
 
-    public function index(){
+    public function index()
+    {
         $judul['title'] = "Kelola Customers";
         $this->load->view('Dashboard/sidebar', $judul);
         $this->load->view('Dashboard/topbar');
         $this->load->view('Customers/content');
         $this->load->view('Dashboard/footer');
+        //
     }
 
-    public function showPelanggan(){
+    public function showPelanggan()
+    {
         $tampilPelanggan = $this->Customers_model->tampilkanSemua();
         echo json_encode($tampilPelanggan);
     }
 
-    public function tambahBaru(){
-		$idPelanggan = $this->input->post('idPelanggan');
-		$namaPelanggan = $this->input->post('namaPelanggan');
-		$daya = $this->input->post('daya');
-		$jenis = $this->input->post('jenis');
-		$data = $this->Customers_model->doneTambahBaru($idPelanggan, $namaPelanggan, $daya, $jenis);
-		echo json_encode($data);
+    public function tambahBaru()
+    {
+        $idPelanggan = $this->input->post('idPelanggan');
+        $namaPelanggan = $this->input->post('namaPelanggan');
+        $daya = $this->input->post('daya');
+        $jenis = $this->input->post('jenis');
+        $data = $this->Customers_model->doneTambahBaru($idPelanggan, $namaPelanggan, $daya, $jenis);
+        echo json_encode($data);
     }
 
-    public function hapusData(){
+    public function hapusData()
+    {
         $id = $this->input->post('kode');
         $data = $this->Customers_model->doneHapusData($id);
         echo json_encode($data);
     }
 
-    public function detailPelanggan(){
-		$idne = $this->input->get('id');
-		$data = $this->Customers_model->getDetailPelanggan($idne);
-		echo json_encode($data);
+    public function detailPelanggan()
+    {
+        $idne = $this->input->get('id');
+        $data = $this->Customers_model->getDetailPelanggan($idne);
+        echo json_encode($data);
     }
 
-    public function updatePelanggan(){
-		$idPelanggan = $this->input->post('idPelanggan_e');
-		$namaPelanggan = $this->input->post('namaPelanggan_e');
-		$daya = $this->input->post('daya_e');
+    public function updatePelanggan()
+    {
+        $idPelanggan = $this->input->post('idPelanggan_e');
+        $namaPelanggan = $this->input->post('namaPelanggan_e');
+        $daya = $this->input->post('daya_e');
         $jenis = $this->input->post('jenis_e');
         $bulanIni = $this->input->post('bulanIni');
-		$data=$this->Customers_model->startUpdatePelanggan($idPelanggan, $namaPelanggan, $daya, $jenis, $bulanIni);
-		echo json_encode($data);
-	}
+        $data = $this->Customers_model->startUpdatePelanggan($idPelanggan, $namaPelanggan, $daya, $jenis, $bulanIni);
+        echo json_encode($data);
+    }
 }
